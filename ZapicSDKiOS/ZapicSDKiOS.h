87b0b42e0e95b3e84ebe326d23b21b362c221826
Daniel Sarfati <daniel@zapic.com>
2017-06-30 12:43:44 -0700

    Initial Commit

---
+++ ZapicSDKiOS/ZapicSDKiOS.h
@@ -0,0 +1,19 @@
+//
+//  ZapicSDKiOS.h
+//  ZapicSDKiOS
+//
+//  Created by Daniel Sarfati on 6/30/17.
+//  Copyright © 2017 Zapic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZapicSDKiOS.
+FOUNDATION_EXPORT double ZapicSDKiOSVersionNumber;
+
+//! Project version string for ZapicSDKiOS.
+FOUNDATION_EXPORT const unsigned char ZapicSDKiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZapicSDKiOS/PublicHeader.h>
+
+
