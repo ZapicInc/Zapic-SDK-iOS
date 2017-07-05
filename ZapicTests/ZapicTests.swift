d66a2c912f326fe7d1fb675c2973bda2ac9484a4
Daniel Sarfati <daniel@zapic.com>
2017-07-05 15:23:14 -0700

    Unit tests will always fail (testing travis)

--- ZapicTests/ZapicTests.swift
+++ ZapicTests/ZapicTests.swift
@@ -24,6 +24,7 @@ class ZapicTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(1 == 2)
     }
     
     func testPerformanceExample() {
