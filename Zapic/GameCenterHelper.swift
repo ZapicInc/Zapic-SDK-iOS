1ada97af2e18e5e6cf10088c391d99da85544eea
Daniel Sarfati <daniel@zapic.com>
2017-08-18 15:43:35 -0700

    Added DisplayName to GameCenter auth signature

--- Zapic/GameCenterHelper.swift
+++ Zapic/GameCenterHelper.swift
@@ -96,6 +96,7 @@ class GameCenterHelper {
       let timestampStr = String(timestamp)
 
       let dict = ["playerId": player.playerID ?? "",
+                  "displayName": player.displayName ?? "",
                   "bundleId": Bundle.main.bundleIdentifier ?? "",
                   "publicKeyUrl": publicKeyUrl.absoluteString,
                   "signature": signatureStr,
